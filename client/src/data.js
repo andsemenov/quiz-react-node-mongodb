23d56cf27636b690275824873ae3147a60383016
Andrei <andsemenov@gmail.com>
2021-06-09 10:44:00 +0100

    created question form

---
+++ client/src/data.js
@@ -0,0 +1,26 @@
+const questions = [
+  {
+    id: 1,
+    question: "How many oscars did the Titanic movie got?",
+    options: ["11", "0", "13", "26"],
+    rightAnswer: 0,
+  },
+  {
+    id: 2,
+    question: "What is the house number of the Simpsons?",
+    options: ["Number 1742", "Number 742", "Number 35", "Number 0"],
+    rightAnswer: 1,
+  },
+  {
+    id: 3,
+    question: 'Who played Che Guevara in the movie "Evita?"',
+    options: [
+      "Arnold Schwarzenegger",
+      "Al Pacino",
+      "Woody Allen",
+      "Antonio Banderas",
+    ],
+    rightAnswer: 3,
+  },
+];
+export default questions;
