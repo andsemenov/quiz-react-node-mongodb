20d43cabbd135e8a32a651fef4db75eb39f03439
Andrei <andsemenov@gmail.com>
2021-06-13 20:46:55 +0100

    added some styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import QuestionForm from "./QuestionForm";
+import QuestionForm from "./QuestionForm/index";
 
 const App = () => {
   const [questions, setQuestions] = useState([]);
