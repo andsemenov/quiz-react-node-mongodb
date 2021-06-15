51ab3acea1fd8165db942029e6365a22d3ade21c
Andrei <andsemenov@gmail.com>
2021-06-15 22:57:03 +0100

    added routing to quiz

--- client/src/components/QuestionForm/index.js
+++ client/src/components/QuestionForm/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Button, Form, Radio, Divider } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
+import Result from "../Result";
 import "./style.css";
 
 const QuestionForm = (props) => {
   const [counterQuestion, setCounterQuestion] = useState(1);
   const [selectedAnswer, setSelectedAnswer] = useState("");
-
   console.log(selectedAnswer);
 
   const handleChange = (event, { value }) => {
@@ -14,35 +15,45 @@ const QuestionForm = (props) => {
 
   return (
     <>
-      <Form
-        className="question-form"
-        onSubmit={() => {
-          setSelectedAnswer("");
-          setCounterQuestion((prev) => prev + 1);
-        }}
-      >
-        <p>
-          Question {counterQuestion} from {props.questions.length}
-        </p>
-        <Divider fitted />
-        <p>{props.questions[counterQuestion - 1].question}</p>
-        {props.questions[counterQuestion - 1].options.map((option, index) => {
-          return (
-            <Form.Field key={index}>
-              <Radio
-                label={option}
-                value={option}
-                checked={selectedAnswer === option}
-                disabled={selectedAnswer !== ""}
-                onChange={handleChange}
-              />
-            </Form.Field>
-          );
-        })}
-        <Button id="btn-next" type="submit">
-          Next
-        </Button>
-      </Form>
+      {counterQuestion <= props.questions.length ? (
+        <Form
+          className="question-form"
+          onSubmit={() => {
+            setSelectedAnswer("");
+            setCounterQuestion((prev) => prev + 1);
+          }}
+        >
+          <p>
+            Question {counterQuestion} from {props.questions.length}
+          </p>
+          <Divider fitted />
+          <p>{props.questions[counterQuestion - 1].question}</p>
+          {props.questions[counterQuestion - 1].options.map((option, index) => {
+            return (
+              <Form.Field key={index}>
+                <Radio
+                  label={option}
+                  value={option}
+                  checked={selectedAnswer === option}
+                  disabled={selectedAnswer !== ""}
+                  onChange={handleChange}
+                />
+              </Form.Field>
+            );
+          })}
+          <Button id="btn-next" type="submit">
+            Next
+          </Button>
+        </Form>
+      ) : (
+        /*      <Redirect to="/result" /> */
+        <Redirect
+          to={{
+            pathname: "/result",
+            props: { answered: "123", questions: props.questions.length },
+          }}
+        />
+      )}
     </>
   );
 };
